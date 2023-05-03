8dfc2a1c6c119e330b02566aa4cf5645b96e9b0b
rajatmaini <37542727+rajatmaini@users.noreply.github.com>
2023-05-03 22:35:32 +0530

    First Commit

---
+++ app/src/main/java/com/example/travelnexus/FlightSearch.java
@@ -0,0 +1,14 @@
+package com.example.travelnexus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlightSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_search);
+    }
+}
